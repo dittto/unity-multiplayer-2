2c8a9f4eef24404c3da62f2daa80f6130e9d8995
Nick Stacey <nick@ditt.to>
2017-02-23 23:38:18 +0000

    Initial import

---
+++ Assets/Player/SyncedData/LocalPlayerDataManager.cs
@@ -0,0 +1,28 @@
+﻿// Player/SyncedData/LocalPlayerDataManager.cs
+
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Player.SyncedData {
+    public class LocalPlayerDataManager : NetworkBehaviour {
+        public PlayerDataForClients localData;
+
+        public override void OnStartLocalPlayer()
+        {
+            base.OnStartLocalPlayer();
+
+            LocalPlayerDataStore store = LocalPlayerDataStore.GetInstance();
+            if (store.playerColour == new Color(0, 0, 0, 0)) {
+                store.playerColour = Random.ColorHSV();
+            }
+
+            localData.SetPlayerColour(store.playerColour);
+            localData.OnPlayerColourUpdated += OnPlayerColourUpdated;
+        }
+
+        public void OnPlayerColourUpdated(Color newColour)
+        {
+            LocalPlayerDataStore.GetInstance().playerColour = newColour;
+        }
+    }
+}
---
+++ Assets/Player/SyncedData/PlayerDataForClients.cs
@@ -0,0 +1,52 @@
+﻿// Player/SyncedData/PlayerDataForClients.cs
+
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Player.SyncedData {
+    public class PlayerDataForClients : NetworkBehaviour {
+        public delegate void PlayerColourUpdated (Color newColour);
+        public event PlayerColourUpdated OnPlayerColourUpdated;
+
+        [SyncVar(hook = "UpdatePlayerColour")]
+        private Color playerColour;
+
+        // use this for re-triggering the hooks on scene load
+        public override void OnStartClient()
+        {
+            base.OnStartClient();
+            // don't update for local player as handled by LocalPlayerOptionsManager
+            // don't update for server as only the clients need this
+            if (!isLocalPlayer && !isServer) {
+                UpdatePlayerColour(playerColour);
+            }
+        }
+
+        // use draw.io to create diagram for this pattern
+
+        // also use draw.io to create diagram for showing how all 3 synced options work together when a scene loads
+
+        [Client]
+        public void SetPlayerColour(Color newColour)
+        {
+            CmdSetPlayerColour(newColour);
+        }
+
+        [Command]
+        public void CmdSetPlayerColour(Color newColour)
+        {
+            playerColour = newColour;
+        }
+
+        [Client]
+        public void UpdatePlayerColour(Color newColour)
+        {
+            playerColour = newColour;
+            GetComponentInChildren<MeshRenderer>().material.color = newColour;
+
+            if (this.OnPlayerColourUpdated != null) {
+                this.OnPlayerColourUpdated(newColour);
+            }
+        }
+    }
+}
